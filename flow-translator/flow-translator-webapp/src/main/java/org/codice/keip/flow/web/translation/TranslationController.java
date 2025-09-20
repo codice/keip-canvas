54cadcae45c96adc2aeb496806d74f57fe2462eb
Abdullah Asaad <abdullah.asaad@connexta.com>
2025-09-19 23:05:05 -0700

    WIP add xml to flow support to webapp

--- flow-translator/flow-translator-webapp/src/main/java/org/codice/keip/flow/web/translation/TranslationController.java
+++ flow-translator/flow-translator-webapp/src/main/java/org/codice/keip/flow/web/translation/TranslationController.java
@@ -1,8 +1,12 @@
 package org.codice.keip.flow.web.translation;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
 import org.codice.keip.flow.model.Flow;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,15 +27,29 @@ class TranslationController {
     this.flowTranslationService = flowTranslationService;
   }
 
-  @Operation(summary = "Translate an EIP flow json to a Spring Integration XML")
+  @Operation(summary = "Translate an EIP Flow json to a Spring Integration XML")
   @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-  ResponseEntity<TranslationResponse> flowToXml(
+  ResponseEntity<TranslationResponse<String>> flowToXml(
       @RequestBody Flow eipFlow, @RequestParam(defaultValue = "false") boolean prettyPrint) {
-    TranslationResponse response = this.flowTranslationService.toXml(eipFlow, prettyPrint);
+    TranslationResponse<String> response = this.flowTranslationService.toXml(eipFlow, prettyPrint);
     if (response.error() == null) {
       return ResponseEntity.ok(response);
     } else {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+  }
+
+  @Operation(summary = "Translate a Spring Integration XML to an EIP Flow json")
+  @PostMapping(consumes = APPLICATION_XML_VALUE, produces = APPLICATION_JSON_VALUE)
+  ResponseEntity<TranslationResponse<Flow>> xmlToFlow(HttpServletRequest request)
+      throws IOException {
+    try (InputStream body = request.getInputStream()) {
+      TranslationResponse<Flow> response = this.flowTranslationService.fromXml(body);
+      if (response.error() == null) {
+        return ResponseEntity.ok(response);
+      } else {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+      }
     }
   }
 }
